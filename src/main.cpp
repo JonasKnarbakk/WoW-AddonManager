fd53e954c936fea1e579c4a5a67d90490061fa4d
Jonas Knarbakk <jonas@knarbakk.no>
2018-03-10 02:10:20 +0100

    Made qt run again by commenting out gtk related code

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,13 @@
 //
 // Author: Jonas Knarbakk
 
-#include "GUI.hpp"
+// #include "GUI.hpp"
 #include <string>
 #include <sstream>
 #include <vector>
-#include "Addon.hpp"
-#include "Core.hpp"
+// #include "Addon.hpp"
+// #include "Core.hpp"
+#include <iostream>
 #include <algorithm>
 #include "WoW-Addon-Manager.hpp"
 #include "mainwindow.h"
@@ -23,53 +24,57 @@ int main(int argc, char * argv[]){
 		<< PROJECT_VERSION_MINOR << "." << PROJECT_VERSION_HOTFIX << std::endl;
 	std::cout << project_string.str();
 
-	if(argc > 1){
-		Core::checkSettings();
-		std::string arg = argv[1];
-		if(arg.compare("gui") == 0){
-			GUI app(&argc, argv);
-		} else if (arg.compare("qt") == 0) {
-			QApplication app(argc, argv);
-			MainWindow mainWindow;
-			mainWindow.show();
-			return app.exec();
-		} else {
-			std::string arg1 = argv[1];
-			if(arg1.compare("search") == 0){
-				std::vector<Addon>searchResults;
-				std::string searchQuery;
-				for(int i = 2; i < argc; i++){
-					std::string arg = argv[i];
-					searchQuery += arg + " ";
-				}
-				searchResults = Core::search(searchQuery);
-				std::vector<Addon>::iterator it;
-				std::sort(searchResults.begin(), searchResults.end(), Core::sortByDownloads);
-				for(it = searchResults.begin(); it != searchResults.end(); ++it){
-					std::cout << *it << "\n" << std::endl;
-				}
-			} else if (arg.compare("list") == 0){
-				Core::list();
-			} else if (arg.compare("set-path") == 0){
-				if(argc >= 2){
-					std::string path = argv[2];
-					Core::setInstallPath(path);
-					std::cout << Core::getInstallPath() << std::endl;
-				}
-			} else if (arg.compare("install") == 0){
-				std::cout << Core::getInstallPath() << std::endl;
-				std::string addon;
-				for(int i = 2; i < argc; i++){
-					std::string arg = argv[i];
-					addon += arg;
-					if(i < argc - 1){
-						addon += "-";
-					}
-				}
-				Core::install(addon);
-			}
-		}
-	}
+	QApplication app(argc, argv);
+	MainWindow mainWindow;
+	mainWindow.show();
+	return app.exec();
+	// if(argc > 1){
+		// Core::checkSettings();
+		// std::string arg = argv[1];
+		// if(arg.compare("gui") == 0){
+			// GUI app(&argc, argv);
+		// } else if (arg.compare("qt") == 0) {
+			// QApplication app(argc, argv);
+			// MainWindow mainWindow;
+			// mainWindow.show();
+			// return app.exec();
+		// } else {
+			// std::string arg1 = argv[1];
+			// if(arg1.compare("search") == 0){
+				// std::vector<Addon>searchResults;
+				// std::string searchQuery;
+				// for(int i = 2; i < argc; i++){
+					// std::string arg = argv[i];
+					// searchQuery += arg + " ";
+				// }
+				// searchResults = Core::search(searchQuery);
+				// std::vector<Addon>::iterator it;
+				// std::sort(searchResults.begin(), searchResults.end(), Core::sortByDownloads);
+				// for(it = searchResults.begin(); it != searchResults.end(); ++it){
+					// std::cout << *it << "\n" << std::endl;
+				// }
+			// } else if (arg.compare("list") == 0){
+				// Core::list();
+			// } else if (arg.compare("set-path") == 0){
+				// if(argc >= 2){
+					// std::string path = argv[2];
+					// Core::setInstallPath(path);
+					// std::cout << Core::getInstallPath() << std::endl;
+				// }
+			// } else if (arg.compare("install") == 0){
+				// std::cout << Core::getInstallPath() << std::endl;
+				// std::string addon;
+				// for(int i = 2; i < argc; i++){
+					// std::string arg = argv[i];
+					// addon += arg;
+					// if(i < argc - 1){
+						// addon += "-";
+					// }
+				// }
+				// Core::install(addon);
+			// }
+		// }
+	// }
 
-	return 0;
+	// return 0;
 }
