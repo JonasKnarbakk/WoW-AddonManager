99f6e3fc7ed742916ff7992622fba4d8724dedbe
Jonas Knarbakk <jonas@knarbakk.no>
2018-03-11 23:38:27 +0100

    Initial work done for doing the search in a separate thread

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,10 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QMessageBox>
-#include <QStandardItemModel>
-#include <QSortFilterProxyModel>
-#include <QtWidgets>
-#include <QTableWidgetItem>
+// #include <QMessageBox>
+// #include <QStandardItemModel>
+// #include <QSortFilterProxyModel>
+// #include <QtWidgets>
+// #include <QTableWidgetItem>
 #include "Core.hpp"
 #include "Addon.hpp"
 #include <iostream>
@@ -14,10 +14,14 @@ MainWindow::MainWindow(QWidget *parent) :
 	QMainWindow(parent),
 	ui(new Ui::MainWindow)
 {
-	curse.updateDatabase();
 	ui->setupUi(this);
 	// Create model
 	model = new QStandardItemModel(0, 0);
+
+	// Prepare the loading icon
+	QMovie *movie = new QMovie("resources/ajax-loader.gif");
+	ui->loadingIcon->setMovie(movie);
+	ui->loadingIcon->setVisible(false);
 
 	QStringList columnNames;
 	columnNames << "Image" << "Name" << "Version" << "Supports" << "Downloads" << " ";
@@ -36,34 +40,91 @@ MainWindow::MainWindow(QWidget *parent) :
 
 MainWindow::~MainWindow()
 {
+	workerThread.quit();
+	workerThread.wait();
 	delete ui;
 }
 
 void MainWindow::on_searchButton_released() {
-	model->setRowCount(0);
+	startSearchThread();
+}
 
-	std::vector<Addon> addons = curse.search(ui->searchField->text().toUtf8().constData());
+void MainWindow::on_searchField_returnPressed() {
+	startSearchThread();
+}
 
-	int i = 0;
-	for(Addon addon : addons) {
+void MainWindow::on_installPathButton_released() {
+	// QFileDialog fileDialog(this);
+	// fileDialog.setFileMode(QFileDialog::DirectoryOnly);
+	// fileDialog.setViewMode(QFileDialog::Detail);
+	QDir defaultPath = QFileDialog::getExistingDirectory(this, tr("Open Directory"),
+			"/home", QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+	ui->installPathField->setText(defaultPath.absolutePath());
+	std::cout << defaultPath.absolutePath().toUtf8().constData() << std::endl;
+}
+
+void MainWindow::downloadAddon() {
+	QObject* obj = sender();
+	QVariant value = obj->property("link");
+	std::string url = value.toString().toUtf8().constData();
+	std::cout << "Download link: " << url << std::endl;
+	Connection conn{};
+	conn.connect(url);
+	conn.save_data_to_file("test.zip");
+}
+
+void Worker::run() {
+	std::cout << "doWork started" << std::endl;
+	/* Expensive operation */
+	Curse curse{};
+	curse.updateDatabase();
+	std::vector<Addon> addons = curse.search(searchParam);
+
+	std::vector<QPixmap> thumbnails;
+	for (Addon addon : addons) {
 		if(addon.getImageLink().compare("Not set") != 0) {
 			// Download the thumbnail image
 			Connection conn{};
 			conn.connect(addon.getImageLink());
 			conn.save_data_to_file("resources/tmp_thumbnail");
-			QLabel* label = new QLabel();
-			label->setPixmap(QPixmap("resources/tmp_thumbnail"));
-			label->setScaledContents(true);
-			model->setItem(i, 0, nullptr);
-			ui->tableView->setIndexWidget(model->index(i, 0), label);
+			QPixmap pixmap("resources/tmp_thumbnail");
+			thumbnails.push_back(pixmap);
 		} else {
 			// If there is no image we will display a happy little penguin for now :)
-			QLabel* label = new QLabel();
-			label->setPixmap(QPixmap("resources/Tux-icon-mono.svg"));
-			label->setScaledContents(true);
-			model->setItem(i, 0, nullptr);
-			ui->tableView->setIndexWidget(model->index(i, 0), label);
+			QPixmap pixmap("resources/Tux-icon-mono.svg");
+			thumbnails.push_back(pixmap);
 		}
+	}
+
+	std::cout << "doWork finished" << std::endl;
+
+	// Pack the addons into a QVariant for sending
+	QVariant variantAddons;
+	variantAddons.setValue(addons);
+	// Do the same to the thumbnails
+	QVariant variantThumbnails;
+	variantThumbnails.setValue(thumbnails);
+	emit resultReady(variantAddons, variantThumbnails);
+}
+
+void MainWindow::handleResults(const QVariant& variantAddons, const QVariant& variantThumbnails) {
+	std::vector<Addon> addons;
+	std::vector<QPixmap> thumbnails;
+	if (variantAddons.canConvert<std::vector<Addon>>()
+			&& variantThumbnails.canConvert<std::vector<QPixmap>>()) {
+		addons = variantAddons.value<std::vector<Addon>>();
+		thumbnails = variantThumbnails.value<std::vector<QPixmap>>();
+	} else {
+		return;
+	}
+	model->setRowCount(0);
+	int i = 0;
+	for(Addon addon : addons) {
+		QLabel* label = new QLabel();
+		label->setPixmap(thumbnails.at(i));
+		label->setScaledContents(true);
+		model->setItem(i, 0, nullptr);
+		ui->tableView->setIndexWidget(model->index(i, 0), label);
 
 		// ui->tableView->setIndexWidget(model->index(i, 0), thumbnail);
 		QStandardItem* name = new QStandardItem(QString("%0").arg(addon.getName().c_str()));
@@ -85,24 +146,29 @@ void MainWindow::on_searchButton_released() {
 	}
 
 	ui->tableView->sortByColumn(4, Qt::DescendingOrder);
+
+	searching = false;
+	showSearchButton();
 }
 
-void MainWindow::on_installPathButton_released() {
-	// QFileDialog fileDialog(this);
-	// fileDialog.setFileMode(QFileDialog::DirectoryOnly);
-	// fileDialog.setViewMode(QFileDialog::Detail);
-	QDir defaultPath = QFileDialog::getExistingDirectory(this, tr("Open Directory"),
-			"/home", QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
-	ui->installPathField->setText(defaultPath.absolutePath());
-	std::cout << defaultPath.absolutePath().toUtf8().constData() << std::endl;
+void MainWindow::startSearchThread() {
+	if(!searching) {
+		showLoadingIcon();
+		Worker* worker = new Worker(ui->searchField->text().toUtf8().constData());
+		connect(worker, &Worker::resultReady, this, &MainWindow::handleResults);
+		connect(worker, &Worker::finished, worker, &QObject::deleteLater);
+		worker->start();
+	}
 }
 
-void MainWindow::downloadAddon() {
-	QObject* obj = sender();
-	QVariant value = obj->property("link");
-	std::string url = value.toString().toUtf8().constData();
-	std::cout << "Download link: " << url << std::endl;
-	Connection conn{};
-	conn.connect(url);
-	conn.save_data_to_file("test.zip");
+void MainWindow::showLoadingIcon() {
+	ui->searchButton->setVisible(false);
+	ui->loadingIcon->setVisible(true);
+	ui->loadingIcon->movie()->start();
+}
+
+void MainWindow::showSearchButton() {
+	ui->searchButton->setVisible(true);
+	ui->loadingIcon->movie()->stop();
+	ui->loadingIcon->setVisible(false);
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,31 +2,61 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QString>
-#include <QStandardItemModel>
+#include <QtWidgets>
+// #include <QStandardItemModel>
+#include <QThread>
 #include <addonsources/curse.hpp>
+#include <QMetaType>
+
+Q_DECLARE_METATYPE(std::vector<Addon>);
+Q_DECLARE_METATYPE(std::vector<QPixmap>);
 
 namespace Ui {
 class MainWindow;
 }
 
+class Worker : public QThread {
+
+	Q_OBJECT
+
+	public:
+		Worker(std::string search) {
+			searchParam = search;
+		}
+	private:
+		std::string searchParam;
+		void run() override;
+	signals:
+	void resultReady(const QVariant& variantAddons, const QVariant& variantThumbnails);
+};
+
 class MainWindow : public QMainWindow
 {
 	Q_OBJECT
-
+	QThread workerThread;
 public:
-	explicit MainWindow(QWidget *parent = 0);
+	explicit MainWindow(QWidget* parent = 0);
 	~MainWindow();
 
+public slots:
+	void handleResults(const QVariant& variantAddons, const QVariant& variantThumbnails);
 private slots:
 	void on_searchButton_released();
+	void on_searchField_returnPressed();
 	void on_installPathButton_released();
 	void downloadAddon();
-	
+
+signals:
+	void operate(const QString &);
+
 private:
-	Ui::MainWindow *ui;
+	Ui::MainWindow* ui;
 	QStandardItemModel* model;
-	Curse curse{};
+	bool searching = false;
+
+	void startSearchThread();
+	void showLoadingIcon();
+	void showSearchButton();
 };
 
 #endif // MAINWINDOW_H
